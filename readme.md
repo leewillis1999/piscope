42c6dce69dae1ed778f5bbe742497ab031eb42a4
leewillis1999 <lee@willissoftware.com>
2021-03-04 11:38:32 +0000

    initial version of files

---
+++ readme.md
@@ -0,0 +1,13 @@
+Python script that runs a web server, and then starts the pi camera taking a picture regularly. Index.html will serve the picture refreshing every second.
+
+This is set to run in /etc/rc.local
+
+The pi can be set to create a wifi hotspot - connect to this with your phone and browse to the server (by IP Address)
+
+### todo
+
+- Make the server handle multiple connections
+- configure the time between pictures
+- make the image zoomable on the web page
+
+
---
+++ webserver.py
@@ -0,0 +1,106 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import threading
+import base64
+from time import sleep
+from picamera import PiCamera
+import os
+import logging
+
+class  web_server(BaseHTTPRequestHandler):
+	def do_GET(self):
+		if self.path == "/":
+			self.path = "/index.html"
+		logging.info ("Serving GET for " + self.path[1:])
+
+		try:
+			reply = False
+			path = os.path.dirname(os.path.realpath(__file__)) + "/"
+			logging.info ("Running in " + path)
+
+			if "/api" in self.path:
+				mime = "text/plain";
+				reply = True
+			elif self.path.endswith(".html"):
+				mime = "text/html"
+				reply = True
+			elif self.path.endswith(".jpg"):
+				mime = "text/jpg"
+				reply = True
+			elif self.path.endswith(".js"):
+				mime = "application/javascript"
+				reply = True
+			elif self.path.endswith(".css"):
+				mime = "text/css"
+				reply = True
+
+			if reply:
+				self.send_response(200)
+				self.send_header("Content-type", mime)
+				self.end_headers()
+
+				if self.path == "/api/getImage":
+					logging.info("returning base64 image")
+					f = open(path + "target.jpg", "rb")
+					self.wfile.write(base64.b64encode(f.read()))
+					f.close()
+				else:
+					logging.info("Sending " + self.path + " - type " + mime)
+					f = open(path + self.path, "rb")
+					self.wfile.write(f.read())
+					f.close()
+			return
+
+		except IOError as e:
+			logging.error(self.path + "  " + str(e))
+			self.send_error(404, "File not found : %s", self.path)
+
+
+def start_web_server():
+	httpd = HTTPServer(("0.0.0.0", 80), web_server)
+	logging.info("Starting web server...")
+	httpd.serve_forever()
+
+def start_camera(path):
+	logging.info ("Initialising camera...")
+	camera = PiCamera(resolution = (1280, 720), framerate = 30)
+	sleep(2)
+	logging.info("Camera initialised")
+	camera.shutter_speed = camera.exposure_speed
+	camera.exposure_mode = "off"
+	g = camera.awb_gains
+	camera.awb_mode = "off"
+	camera.awb_gains = g
+
+	logging.info("Starting capture...")
+	for fn in camera.capture_continuous(path + "/target.jpg"):
+		#print ("Captured %s" % fn)
+		sleep(1)
+
+	logging.info("Finished capture")
+
+try:
+	#configure logging
+	#if os.path.exists("camera.log"):
+	#	os.remove("camera.log")
+	#path = os.path.dirname(os.path.realpath(__file__)) + "/"
+
+	path = os.path.dirname(os.path.realpath(__file__))
+	logfile = path + "/camera.log"
+	logging.basicConfig(
+		level=logging.INFO,
+		format=('[%(asctime)s] %(levelname)-8s %(name)-12s %(message)s'),
+		filename=(logfile)
+	)
+	logging.info("")
+	logging.info("")
+	##logging.info("Here I am ")
+	thread = threading.Thread(target=start_web_server)
+	thread.setDaemon(True)
+	thread.start()
+	logging.info("web server started")
+
+	start_camera(path)
+
+except KeyboardInterrupt:
+	logging.error("Shutting down the web server")
+	httpd.socket_close()
